03ce17164702f1a67d32854476e74f55aaec7bc0
Bas Veeling <basveeling@gmail.com>
2015-06-17 13:58:10 +0200

    Python test

--- moses/FF/VectorSimStatelessFF.cpp
+++ moses/FF/VectorSimStatelessFF.cpp
@@ -2,6 +2,7 @@
 #include "VectorSimStatelessFF.h"
 #include "moses/ScoreComponentCollection.h"
 #include "moses/TargetPhrase.h"
+#include <Python.h>
 #include <boost/python.hpp>
 
 using namespace boost::python;
