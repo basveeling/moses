ce0233b44b760ad0dfcab01372724db837c75f7c
Bas Veeling <basveeling@gmail.com>
2015-06-17 15:08:29 +0200

    fixie

--- moses/FF/VectorSimStatelessFF.cpp
+++ moses/FF/VectorSimStatelessFF.cpp
@@ -11,21 +11,6 @@ VectorSimStatelessFF::VectorSimStatelessFF(const std::string &line)
   :StatelessFeatureFunction(2, line)
 {
   ReadParameters();
-  try {
-    Py_Initialize();
-
-    object main_module((
-      handle<>(borrowed(PyImport_AddModule("__main__")))));
-
-    object main_namespace = main_module.attr("__dict__");
-
-    handle<> ignored(( PyRun_String( "print \"Hello, World\"",
-                                     Py_file_input,
-                                     main_namespace.ptr(),
-                                     main_namespace.ptr() ) ));
-  } catch( error_already_set ) {
-    PyErr_Print();
-  }
 }
 
 void VectorSimStatelessFF::EvaluateInIsolation(const Phrase &source
